6d402e6698c8d5b8a0baa588c058c62d0ade81ff
Olivia L <letendartolivia@gmail.com>
2023-09-21 13:40:37 +0200

    create the new component : RentalPresentation + insert in the router + create css + insert new image in the folder assets

--- Kasa/src/App.jsx
+++ Kasa/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import RentalPresentation from "./components/RentalPresentation/RentalPresentation.jsx";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
             <Routes>
                 {/* Une route logement*/}
+                <Route path='/' element={<RentalPresentation />} />
                 {/*Une route dynamique pour chaque page de logement*/}
                 {/*Page 404 : Route path="*" element={<NotFound />}*/}
             </Routes>
---
+++ Kasa/src/components/RentalPresentation/RentalPresentation.jsx
@@ -0,0 +1,12 @@
+import backgroundImage from '../../assets/backgroundImageRental.png'
+import './RentalPresentation.css'
+export default function RentalPresentation() {
+    return (
+        <div id="parent">
+            <div id="img-background">
+                <p>Chez vous partout et ailleurs</p>
+            </div>
+
+        </div>
+    )
+}
