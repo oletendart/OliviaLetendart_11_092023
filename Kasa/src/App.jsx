70f4e28c60857952c380fc58d66a7be9b8631152
Olivia L <letendartolivia@gmail.com>
2023-09-21 12:37:55 +0200

    Create new components (navbar) and insert in the file App

--- Kasa/src/App.jsx
+++ Kasa/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar.jsx"
 
 
 function App() {
@@ -7,13 +8,15 @@ function App() {
   return (
     <>
         <BrowserRouter>
-            {/* Accueil */}
+            <Navbar />
 
             <Routes>
                 {/* Une route logement*/}
                 {/*Une route dynamique pour chaque page de logement*/}
                 {/*Page 404 : Route path="*" element={<NotFound />}*/}
             </Routes>
+
+            <Footer />
         </BrowserRouter>
     </>
   )
