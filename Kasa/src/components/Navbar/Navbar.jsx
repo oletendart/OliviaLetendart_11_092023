55a6d000ddd85890566ff8eaa50157f11a1d444d
Olivia L <letendartolivia@gmail.com>
2023-09-21 13:10:36 +0200

    create the css fort he component "navvar" and remove the precedent css in the file app.css and index.css

--- Kasa/src/components/Navbar/Navbar.jsx
+++ Kasa/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,20 @@
+import logo from '../../assets/LOGO.png'
+import './Navbar.css'
+
 export default function Navbar() {
     return (
-        <div>Navbar</div>
+        <div id="navbar">
+            <div id="flex-nav">
+            <img src={logo}
+                 alt="Logo du site"/>
+
+            <nav>
+                <ul>
+                    <li>Accueil</li>
+                    <li>A propos</li>
+                </ul>
+            </nav>
+            </div>
+        </div>
     )
 }
