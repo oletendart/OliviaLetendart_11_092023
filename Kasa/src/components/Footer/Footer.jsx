00b56c37c65fe5905db1173b2b2dfe53af92ae1e
Olivia L <letendartolivia@gmail.com>
2023-09-21 13:22:02 +0200

    create the css for the component "footer"

--- Kasa/src/components/Footer/Footer.jsx
+++ Kasa/src/components/Footer/Footer.jsx
@@ -1,5 +1,12 @@
+import logoFooter from '../../assets/LOGO_FOOTER.png'
+import './Footer.css'
+
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <div id="footer">
+            <img src={logoFooter}
+                 alt="Logo dans le footer de la page"/>
+            <p>© 2020 Kasa. All rights reserved</p>
+        </div>
     )
 }
