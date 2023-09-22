b2d1fa7c2e2aaa44c1aed1bf8531b0d7943c3eb5
Olivia L <letendartolivia@gmail.com>
2023-09-22 15:08:49 +0200

    Insert Link in the navbar

--- Kasa/src/layouts/Navbar/Navbar.jsx
+++ Kasa/src/layouts/Navbar/Navbar.jsx
@@ -1,19 +1,30 @@
 import logo from '../../assets/LOGO.png'
 import './Navbar.css'
+import {Link} from "react-router-dom";
 
 export default function Navbar() {
     return (
         <div id="navbar">
             <div id="flex-nav">
-            <img src={logo}
-                 alt="Logo du site"/>
+                <Link to="/">
+                    <img src={logo}
+                         alt="Logo du site"/>
+                </Link>
 
-            <nav>
-                <ul>
-                    <li>Accueil</li>
-                    <li>A propos</li>
-                </ul>
-            </nav>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">
+                                Accueil
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/findoutmore">
+                                A propos
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
     )
---
+++ Kasa/src/pages/FindOutMore/FindOutMore.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../layouts/Navbar/Navbar.jsx";
+import Footer from "../../layouts/Footer/Footer.jsx";
+
+export default function FindOutMore() {
+    return (
+        <>
+        <Navbar />
+            <div>FindOutMore</div>
+            <Footer />
+        </>
+    )
+}
