506ddd0e06293d09a2b53004afa1a324045b1c68
Olivia L <letendartolivia@gmail.com>
2023-09-25 18:10:28 +0200

    create component Dropdown and insert in the page FindOut

---
+++ Kasa/src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,14 @@
+import './Dropdown.css'
+
+export default function Dropdown() {
+    return (
+        <>
+            <div className="t-dropdown-block">
+                <div className="t-dropdown-select">
+                    <p>Equipements</p>
+                    <i className="fas fa-2x fa-chevron-down t-select-btn"></i>
+                </div>
+            </div>
+        </>
+    )
+}
--- Kasa/src/pages/FindOutMore/FindOutMore.jsx
+++ Kasa/src/pages/FindOutMore/FindOutMore.jsx
@@ -1,11 +1,21 @@
 import Navbar from "../../layouts/Navbar/Navbar.jsx";
 import Footer from "../../layouts/Footer/Footer.jsx";
+import './FindOutMore.css'
+import backgroundImage from "../../assets/backgroundFindOut.png";
+import Dropdown from "../../components/Dropdown/Dropdown.jsx";
 
 export default function FindOutMore() {
     return (
         <>
-        <Navbar />
-            <div>FindOutMore</div>
+            <Navbar />
+            <div id="parentFindOut">
+                <img src={backgroundImage}
+                     alt="image background de la bannière" id="background-image-findOut"/>
+            </div>
+
+            <div>
+                <Dropdown />
+            </div>
             <Footer />
         </>
     )
