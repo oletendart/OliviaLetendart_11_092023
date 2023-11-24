994298b417822cd8b6fe1c1445a92d1c881dc1fb
Olivia L <letendartolivia@gmail.com>
2023-11-24 14:59:14 +0100

    responsive caroussel + finish page presentationLogement (css and html)

--- Kasa/src/pages/About/About.jsx
+++ Kasa/src/pages/About/About.jsx
@@ -7,18 +7,20 @@ import Dropdown from "../../components/Dropdown/Dropdown.jsx";
 export default function About() {
     return (
         <>
-            <Navbar />
-            <div id="parentFindOut">
-                <img src={backgroundImage}
-                     alt="image background de la bannière" id="background-image-findOut"/>
-            </div>
+            <section id="parentAbout">
+                <Navbar />
+                <div id="parentFindOut">
+                    <img src={backgroundImage}
+                         alt="image background de la bannière" id="background-image-findOut"/>
+                </div>
 
-            <div className="dropdownBlock">
-                <Dropdown name="Fiabilité" description="123456789"/>
-                <Dropdown name="Respect"  description="hjkhjkhkj"/>
-                <Dropdown name="Service"  description="151464564jkhjkhjkhkj"/>
-                <Dropdown name="Sécurité"  description="tototoototototototototo"/>
-            </div>
+                <div className="dropdownBlock">
+                    <Dropdown name="Fiabilité" description="123456789"/>
+                    <Dropdown name="Respect"  description="hjkhjkhkj"/>
+                    <Dropdown name="Service"  description="151464564jkhjkhjkhkj"/>
+                    <Dropdown name="Sécurité"  description="tototoototototototototo"/>
+                </div>
+            </section>
             <Footer />
         </>
     )
--- Kasa/src/pages/presentationLogement/PresentationLogement.jsx
+++ Kasa/src/pages/presentationLogement/PresentationLogement.jsx
@@ -1,7 +1,5 @@
 import Navbar from "../../layouts/Navbar/Navbar.jsx";
 import Footer from "../../layouts/Footer/Footer.jsx";
-import starActive from '../../assets/starActive.png';
-import starInactive from '../../assets/starInactive.png';
 import imageVendeur from '../../assets/imageVendeur.png';
 import './PresentationLogement.css'
 import Dropdown from "../../components/Dropdown/Dropdown.jsx";
@@ -12,41 +10,42 @@ import Rate from "../../components/Rate/Rate.jsx";
 export default function PresentationLogement() {
     return (
         <>
-        <Navbar />
             <div id="carousselParent">
+                <Navbar />
                 <Caroussel />
 
-                <section id="flexLogement">
-                    <div id="nameLogement">
-                        <h1>
-                            Cozy loft on the Canal Saint-Martin
-                        </h1>
+                <div id="flexParentLogement">
+                    <section id="flexLogement">
+                        <div id="nameLogement">
+                            <h1>
+                                Cozy loft on the Canal Saint-Martin
+                            </h1>
 
-                        <h2>
-                            Paris, Île-de-france
-                        </h2>
-                    </div>
+                            <h2>
+                                Paris, Île-de-france
+                            </h2>
+                        </div>
 
-                    <div className="vendeur">
-                        <h3>
-                            Alexandre Dumas
-                        </h3>
-                        <img src={imageVendeur}
-                             alt="vendeur image"/>
-                    </div>
-                </section>
+                        <div className="tagParent">
+                            <Tag name="cozy"/>
+                            <Tag name="douillet"/>
+                            <Tag name="confort"/>
+                        </div>
+                    </section>
 
-                <section id="flexRateAndTag">
-                    <div className="tagParent">
-                        <Tag name="cozy"/>
-                        <Tag name="douillet"/>
-                        <Tag name="confort"/>
-                    </div>
-
-                    <div id="rate">
-                        <Rate />
-                    </div>
-                </section>
+                    <section id="flexRateAndVendeur">
+                        <div className="vendeur">
+                            <h3>
+                                Alexandre Dumas
+                            </h3>
+                            <img src={imageVendeur}
+                                 alt="vendeur image"/>
+                        </div>
+                        <div id="rate">
+                            <Rate />
+                        </div>
+                    </section>
+                </div>
 
                 <section id="dropdownLogement">
                     <Dropdown name="Description"/>
