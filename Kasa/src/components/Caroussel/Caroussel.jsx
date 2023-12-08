2d1aabaa248912f65fb0a042c471a37e3bb845ad
Olivia L <letendartolivia@gmail.com>
2023-12-08 13:05:47 +0100

    beginning of the carousel logic

--- Kasa/src/components/Caroussel/Caroussel.jsx
+++ Kasa/src/components/Caroussel/Caroussel.jsx
@@ -4,12 +4,46 @@ import arrowPrevious from "../../assets/arrowPrevious.png";
 import arrowNext from "../../assets/arrowNext.png";
 
 export default function Caroussel() {
+
+        const images = [
+            {id: 0, src: imageCaroussel}
+        ]
+
+        const handleNext = () => {
+        }
+
+        const handlePrevious = () => {
+
+        }
+
+
+
     return (
         <>
             <section id="caroussel">
-                <img src={imageCaroussel} alt="image caroussel" className="imageCaroussel"/>
-                <img src={arrowPrevious} alt="previous arrow" className="arrow"/>
-                <img src={arrowNext} alt="next arrow" className="arrow next"/>
+                <img src={arrowPrevious}
+                     alt="previous arrow"
+                     className="arrow"
+                     onClick={handlePrevious}
+                />
+                <img src={arrowNext}
+                     alt="next arrow"
+                     className="arrow next"
+                    onClick={handleNext}
+                />
+
+                <ul className="carousselImage">
+                    {images.map((image, index) => (
+                        <li>
+                            <img
+                                key={index}
+                                src={image.src}
+                                 alt="image caroussel"
+                                 className="imageCaroussel"/>
+                        </li>
+                    ))}
+                </ul>
+
             </section>
         </>
     )
